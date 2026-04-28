655500601b5d9c530708cbd26240607d163bd81d
raynecloudy <hi@rayne.page>
2026-04-27 23:41:20 -0700

    images

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -6,7 +6,9 @@ const Modal = (props: Record<string, any> & {
 }) => {
   return (
     <div className="backdrop-blur-xs backdrop-brightness-50 w-screen h-screen p-2 absolute top-0 left-0 flex items-center justify-center">
-      <Box className="p-2 rounded-2xl w-30">{props.children}</Box>
+      <Box className="p-2 rounded-2xl w-30" style={{
+        boxShadow: "0 0.5rem 2rem #000b"
+      }}>{props.children}</Box>
     </div>
   );
 };
--- src/components/SignUpModal/SignUpModal.tsx
+++ src/components/SignUpModal/SignUpModal.tsx
@@ -3,26 +3,45 @@
 import Modal from "../Modal/Modal";
 
 const SignUpModal = () => {
+  const pictures = 10;
+
+  let picture1 = Math.floor(Math.random() * pictures);
+  let picture2;
+  do {
+    picture2 = Math.floor(Math.random() * pictures);
+  } while (picture1 === picture2);
   return (
-    <Modal>
-      <img className="w-12 m-auto" src="/Weathercord.svg" alt="Weathercord" />
-      <h1 className="text-center">Join Weathercord</h1>
-      <form>
-        <label>
-          <div>Username</div>
-          <input type="text" />
-        </label>
-        <label>
-          <div>Password</div>
-          <input type="password" />
-        </label>
-        <label>
-          <div>Re-type Password</div>
-          <input type="password" />
-        </label>
-        <input type="submit" value="Create Account" />
-      </form>
-    </Modal>
+    <>
+      <Modal>
+        <img className="rounded-2xl absolute z-[-1] -translate-x-1/2 -translate-y-1/2 transition hover:scale-105" src={"/signup/" + picture1 + ".png"} aria-hidden style={{
+          top: `calc(55% + ${Math.random() * 10}%)`,
+          left: "calc(50% - 15rem)",
+          width: `${Math.random() * 7 + 14}rem`
+        }} />
+        <img className="rounded-2xl absolute z-[-1] -translate-x-1/2 -translate-y-1/2 transition hover:scale-105" src={"/signup/" + picture2 + ".png"} aria-hidden style={{
+          top: `calc(35% + ${Math.random() * 10}%)`,
+          left: "calc(50% + 15rem)",
+          width: `${Math.random() * 7 + 14}rem`
+        }} />
+        <img className="w-12 m-auto" src="/Weathercord.svg" alt="Weathercord" />
+        <h1 className="text-center">Join Weathercord</h1>
+        <form>
+          <label>
+            <div>Username</div>
+            <input type="text" />
+          </label>
+          <label>
+            <div>Password</div>
+            <input type="password" />
+          </label>
+          <label>
+            <div>Re-type Password</div>
+            <input type="password" />
+          </label>
+          <input type="submit" value="Create Account" />
+        </form>
+      </Modal>
+    </>
   );
 };
 
