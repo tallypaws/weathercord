68fc8d09f047225621f8eaf3998fa8605e69cda7
raynecloudy <hi@rayne.page>
2026-05-03 11:22:27 -0700

    tab scrolling

--- src/components/AccountSettingsModal/AccountSettingsModal.tsx
+++ src/components/AccountSettingsModal/AccountSettingsModal.tsx
@@ -6,7 +6,8 @@ import Modal from "../Modal/Modal";
 import ProfileTab from "./ProfileTab";
 
 export enum Tab {
-  Profile = 0
+  Profile = 0,
+  Connections = 1
 };
 
 const AccountSettingsModal = (props: {
@@ -19,7 +20,7 @@ const AccountSettingsModal = (props: {
 
   return (
     <Modal className="w-65 h-40 flex gap-2">
-      <div className="w-12">
+      <div className="w-13 shrink-0 -m-2 p-2 pr-1 -mr-1 overflow-auto">
         {Object.keys(Tab).map((t) => {
           if (isNaN(parseInt(t))) return (
             <button key={t} className={"w-full text-left rounded-xl cursor-pointer transition hover:bg-(--box-button-active)" + (Tab[tab] === t ? " text-(--accent) bg-(--accent-background)!" : "")} onClick={() => setTab(Tab[t as keyof typeof Tab])} style={{
@@ -28,7 +29,7 @@ const AccountSettingsModal = (props: {
           )
         })}
       </div>
-      <div className="grow overflow-auto -m-2 p-2">
+      <div className="grow overflow-auto -m-2 p-2 pl-1 -ml-1">
         {tab === Tab.Profile &&
           <ProfileTab account={props.account} setAccount={props.setAccount} />
         }
